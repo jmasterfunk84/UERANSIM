63fd065961f41141ade3fe891f6e9bf741de75c0
jmasterfunk84 <48972964+jmasterfunk84@users.noreply.github.com>
2024-01-18 21:14:56 -0600

    Move SMS to a file.

---
+++ src/ue/nas/mm/sms.cpp
@@ -0,0 +1,30 @@
+//
+// This file is a part of UERANSIM project.
+// Copyright (c) 2023 ALİ GÜNGÖR.
+//
+// https://github.com/aligungr/UERANSIM/
+// See README, LICENSE, and CONTRIBUTING files for licensing details.
+//
+
+#include "mm.hpp"
+
+#include <algorithm>
+
+#include <lib/nas/utils.hpp>
+#include <ue/app/task.hpp>
+#include <ue/nas/sm/sm.hpp>
+#include <ue/rrc/task.hpp>
+#include <utils/common.hpp>
+
+namespace nr::ue
+{
+
+void NasMm::sendSMS()
+{
+    if (m_mmState == EMmState::MM_NULL)
+        return;
+
+    m_logger->debug("We should send an SMS");
+}
+
+} // namespace nr::ue
